5f7b1a9f8fe6262e9f3eb9458754d9f0abb00fe9
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-07-12 16:57:30 -0500

    got titles and images from reddit

--- list.js
+++ list.js
@@ -1,11 +1,30 @@
-var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            myObj = JSON.parse(this.responseText);
-            document.getElementById("reddit-posts").innerHTML = myObj;
+var redditPosts = document.querySelector('#reddit-posts');
+var request = new Request('https://www.reddit.com/r/memes.json');
+
+fetch(request).then(function(res){
+    if(res.status !== 200){
+        console.log('Looks like there was a problem');
+        return;
+    }
+res.json().then(function(success){
+
+    var children = success.data.children;
+    for(var i = 0; i < children.length; i++){
+        var i = children.indexOf();
+        if(i != -1) {
+	        children.splice(i, 1);
         }
+        var title = success.data.children[i].data.title;
+        title= title.replace(new RegExp('cum', 'g'), 'applesauce');
+        console.log(title);
+        var titleDiv = document.createElement('div');
+        redditPosts.appendChild(titleDiv);
+        titleDiv.innerHTML = title;
 
-}
-
-    xhttp.open("GET", "https://www.reddit.com/r/NPR/.json", true);
-    xhttp.send('');
+        var imgDiv = document.createElement('IMG');
+        imgDiv.src = success.data.children[i].data.url;
+        redditPosts.appendChild(imgDiv);
+        imgDiv.style.height = '200px';
+    }
+})
+})
